9d0b111784cfab501c18147dc6aef52882e5ca7f
wstgtime <denk174286@outlook.com>
2024-02-17 14:26:53 +0800

    install and set up mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
