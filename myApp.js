fca01e41346d99b543cb6eec7f06a854c2fa4ffc
wstgtime <denk174286@outlook.com>
2024-02-25 21:31:16 +0800

    find person by id and remove

--- myApp.js
+++ myApp.js
@@ -110,7 +110,12 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findOneAndRemove({ _id: personId }, function(err, removedPerson) {
+    if (err) {
+      return console.error(err);
+    }
+    done(null, removedPerson);
+  });
 };
 
 const removeManyPeople = (done) => {
